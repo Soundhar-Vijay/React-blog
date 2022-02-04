6b26997b137be128616a5c046546f85caded5f1d
soundharvijay <soundharsrti@gmail.com>
2022-02-05 01:43:21 +0530

    first commit

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='home-header'>
+    <h2>Simple React Blog</h2>
+    <h1>
+      <span>“</span> Blog <span>”</span>
+    </h1>
+    <p>
+      Blog using React <br /> 
+      A MERN STACK DEVELOPER.
+    </p>
+  </header>
+);
+
+export default Header;
---
+++ src/components/Home/SearchBar/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const SearchBar = ({ formSubmit, value, handleSearchKey, clearSearch }) => (
+  <div className='searchBar-wrap'>
+   
+  </div>
+);
+
+export default SearchBar;
